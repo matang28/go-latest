2e5cd6e577a3ec75c264a97c26eb7d513f0de33a
matang <matang28@gmail.com>
2020-01-30 13:24:58 +0200

    Fixing failing tests on windows

--- lib/discovery_test.go
+++ lib/discovery_test.go
@@ -2,23 +2,28 @@ package lib
 
 import (
 	"github.com/stretchr/testify/assert"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
 func TestDiscoverFilesByExt_FoundModFiles(t *testing.T) {
-	files, err := DiscoverFilesByExt("../test_data/", "go.mod")
+	files, err := DiscoverFilesByExt(filepath.Join("..", "test_data"), "go.mod")
 	assert.Nil(t, err)
 	assert.NotNil(t, files)
 
 	assert.EqualValues(t, 4, len(files))
-	assert.Contains(t, files, "../test_data/go.mod")
-	assert.Contains(t, files, "../test_data/one/go.mod")
-	assert.Contains(t, files, "../test_data/one/one_sub/go.mod")
-	assert.Contains(t, files, "../test_data/two/go.mod")
+	assert.Contains(t, files, filepath.Join("..", "test_data", "go.mod"))
+	assert.Contains(t, files, filepath.Join("..", "test_data", "one", "go.mod"))
+	assert.Contains(t, files, filepath.Join("..", "test_data", "one", "one_sub", "go.mod"))
+	assert.Contains(t, files, filepath.Join("..", "test_data", "two", "go.mod"))
 }
 
 func TestDiscoverFilesByExt_NotFoundModFiles(t *testing.T) {
-	files, err := DiscoverFilesByExt("./", "go.mod")
+	pwd, err := os.Getwd()
+	assert.Nil(t, err)
+
+	files, err := DiscoverFilesByExt(pwd, "go.mod")
 	assert.Nil(t, err)
 	assert.Empty(t, files)
 }
